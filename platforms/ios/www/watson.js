f38796af607394350d8593981da9170d5ba92c26
William Yaworsky <will.yaworsky@gmail.com>
2016-04-24 13:56:45 -0400

    cordova asd

--- platforms/ios/www/watson.js
+++ platforms/ios/www/watson.js
@@ -11,23 +11,16 @@ $.post('https://energy-helmet.herokuapp.com/watson-key').then(function(data) {
 
   var currentZoom = undefined;
 
-  console.log(1)
-
   stream.on('error', function(err) {
-    console.log(err)
-  })
+    console.log(err);
+  });
 
   stream.on('data', function(data) {
-    console.log(2)
     for (var i in data.alternatives) {
-      console.log(3)
       var alt = data.alternatives[i];
       if (data.final) {
-        console.log(4)
         var text = alt.transcript;
-        console.log(text);
         if (/zoom/i.test(text)) {
-          console.log('ZOOMING ', text);
           if (currentZoom) {
             if (currentZoom == 'map')      unZoomMap();
             if (currentZoom == 'slack')    unZoomSlack();
