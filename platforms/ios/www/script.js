fb75152a764c2dfbcb370a33ef11561d0be87d15
William Yaworsky <will.yaworsky@gmail.com>
2016-04-24 12:13:31 -0400

    sd

--- platforms/ios/www/script.js
+++ platforms/ios/www/script.js
@@ -1,29 +1,11 @@
-// var addWidget = (function() {
-//   var x = {
-//     top: { bottom: '20px' },
-//     bottom: { top: '20px' }
-//   };
-//   var y = {
-//     left: { left: '20px' },
-//     right: { right: '20px' }
-//   };
-//   var i = 0;
-//   return function(yPos, xPos, template, updateInterval, render) {
-//     var widgetId         = 'widget-' + i++;
-//     var templateFunction = _.template($(template).html);
-//     var widget           = $('<div id="' + widgetId + '">');
-//     widget.css(y[yPos]);
-//     widget.css(x[xPos]);
-
-//     $('body').append(widget);
-
-//     (function loop() {
-//       render();
-//     })();
-//   };
-// })();
+//
+// MAP
+//
 
 function zoomMap() {
+  $('#template-container').hide();
+  $('.gifz').hide();
+
   $('.map-container').css({
     bottom:   '0',
     right:  '0',
@@ -45,6 +27,9 @@ function zoomMap() {
 }
 
 function unZoomMap() {
+  $('#template-container').show();
+  $('.gifz').show();
+
   $('.map-container').css({
     bottom:   '20px',
     right:  '20px',
@@ -65,9 +50,74 @@ function unZoomMap() {
   });
 }
 
+//
+// SLACK
+//
+
+function zoomSlack() {
+  $('.map-container').hide();
+  $('.gifz').hide();
+
+  $('#template-container').css({
+    bottom: '0',
+    left:  '0',
+    'font-size': '34px'
+  });
+}
+
+function unZoomSlack() {
+  $('.map-container').show();
+  $('.gifz').show();
+
+  $('#template-container').css({
+    bottom: '20px',
+    left:   '20px',
+    'font-size': '14px'
+  });
+}
+
+//
+// GIFZ
+//
+
+function zoomGifz() {
+  $('.map-container').hide();
+  $('#template-container').hide();
+
+  $('.gifz-gif img').css({
+    top: 0,
+    right: 0,
+    height: '100%',
+    width: 'auto'
+  });
+  $('.gifz').css({
+    bottom: '0',
+    right:  '0',
+    height: '100%',
+    width:  'auto'
+  });
+}
+
+function unZoomGifz() {
+  $('.map-container').show();
+  $('#template-container').show();
+
+  $('.gifz-gif img').css({
+    top: 0,
+    right: 0,
+    height: '100px',
+    width: '100px'
+  });
+  $('.gifz').css({
+    top: '20px',
+    right:   '20px',
+    height: '100px',
+    width: '100px'
+  });
+}
 
 
-zoomMap()
+zoomGifz()
 setTimeout(function() {
-  unZoomMap()
+  unZoomGifz()
 }, 5000)
